2342bbb2377e11fdc7112a7352057a2410d79982
madhav-bokare <madhavbokare926@gmail.com>
2026-01-10 17:07:46 +0530

    Update CORS origin for middleware

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB()
 // ===== Middleware =====
 app.use(
   cors({
-    origin: "https://anime-library-sooty.vercel.app",
+    origin: "https://anime-library-zeta.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
